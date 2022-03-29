26a865ce84a9d2fc9d1c4c4411d1743a4e3e2eb4
wndlovu <wndlovu@smith.edu>
2022-03-28 23:29:52 -0400

    -README file

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -3,3 +3,7 @@ rbiWy_dfAll <-
   read_csv()
 usethis::use_data(rbiWy_dfAll, overwrite = TRUE)
 
+all_sites <- as.data.frame(colnames(rbiWy_dfAll)) %>%
+  rename(site_no = `colnames(rbiWy_dfAll)`) %>%
+  filter(!site_no == "waterYear")
+usethis::use_data(all_sites, internal = TRUE)
