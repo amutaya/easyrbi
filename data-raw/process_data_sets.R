c59fc7b5e80d47993c076181b6599ea314848d33
wndlovu <wndlovu@smith.edu>
2022-03-30 06:55:15 -0400

    - passed error check

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -1,6 +1,6 @@
-rbiWy_dfAll <-
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMoc3ICoJarNy8yvsDt3M8voFPLr0jDkORQ4g1e59GnsRiOEpKnMKEyRBsN-nLzT40QXj9ZSiW5dkz/pub?gid=441939603&single=true&output=csv" %>%
-  read_csv()
+library(readr)
+
+rbiWy_dfAll <- read_csv("data-raw/rbiWy_dfAll.csv")
 usethis::use_data(rbiWy_dfAll, overwrite = TRUE)
 
 all_sites <- as.data.frame(colnames(rbiWy_dfAll)) %>%
--- tests/testthat/test_trends.R
+++ tests/testthat/test_trends.R
@@ -3,9 +3,7 @@ library(testthat)
 library(easyrbi)
 library(tidyr)
 
-rbiWy_dfAll <-
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMoc3ICoJarNy8yvsDt3M8voFPLr0jDkORQ4g1e59GnsRiOEpKnMKEyRBsN-nLzT40QXj9ZSiW5dkz/pub?gid=441939603&single=true&output=csv" %>%
-  readr::read_csv()
+
 all_sites <- as.data.frame(colnames(rbiWy_dfAll)) %>%
   dplyr::rename(site_no = `colnames(rbiWy_dfAll)`) %>%
   filter(!site_no == "waterYear")
