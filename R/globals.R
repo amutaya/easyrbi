b2676f5b0a3b2a30627f383eac89c2c12a8f3013
wndlovu <wndlovu@smith.edu>
2022-04-22 21:59:12 -0400

    added variables from the rbi_df function to globals (o errors, 1 warning, 0 note)

--- R/globals.R
+++ R/globals.R
@@ -4,4 +4,5 @@ utils::globalVariables(c("site_values", "all_site_values", "qdiff", "mm_day",
                          "coord_datum_cd", "dec_coord_datum_cd", "district_cd",
                          "state_cd", "county_cd", "alt_va", "drain_area_va",
                          "X_00060_00003", "data.name", "colnames(dataFile)",
-                         "site_no", "tau", "conf.int2", "dec_long_va", "country_cd"))
+                         "site_no", "tau", "conf.int2", "dec_long_va", "country_cd", "data",
+                         "waterYear", "rbi_vals", "rbi_value", "rbi_values"))
