4ff3ca85d29d01f7018368b37b512471effdcb4f
wndlovu <wndlovu@smith.edu>
2022-04-25 21:02:11 -0400

    function tests

--- tests/testthat/test_trends.R
+++ tests/testthat/test_trends.R
@@ -3,5 +3,17 @@ library(testthat)
 library(easyrbi)
 library(tidyr)
 
+rbiWy_df <- rbiWy_dfAll %>%
+  pivot_wider(names_from = "site_no", values_from = rbi)
+
+
+# trends
+expect_equal(ncol(trends(rbiWy_df)), 15)
+expect_true(all(trends(rbiWy_df)$p.value <= 1))
+expect_type(trends(rbiWy_df)$conf.int1, "double")
+
+
+
+
 
 
