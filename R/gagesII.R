5b69e17a0a5bad291f247ac0fa1d83a088a8a49d
wndlovu <wndlovu@smith.edu>
2022-05-04 17:13:39 -0400

    - cleaning out commented out code - fixed basin id to include example

--- R/gagesII.R
+++ R/gagesII.R
@@ -16,29 +16,13 @@
 #' "01570000", "01571500", "01573000", "01574000", "01574500", "01576500")
 #' dam_removal(study_sites)
 
-#dam_removal <- function(site_num){
-  #dam_url <- "https://www.sciencebase.gov/catalog/file/get/59692a64e4b0d1f9f05fbd39?f=__disk__c9%2F40%2Ff2%2Fc940f2f914e04b26bf0e81d81c9f273113f7a5c8"
-  #dam_data <- use_zip(
-    #dam_url,
-    #destdir = getwd(),
-    #cleanup = if (rlang::is_interactive()) NA else FALSE
-  #)
-
-  #dams <- read.table("Dataset3_DamRemovals/DamRemovals.txt",sep=",",header=T) %>%
-    #mutate(STAID = as.character(paste0("0", STAID))) %>%
-    #filter(STAID == site_num) %>%
-    #as_tibble()
-  #return(dams)
-#}
-
 dam_removal <- function(site_num){
   dam_url <- "https://www.sciencebase.gov/catalog/file/get/59692a64e4b0d1f9f05fbd39?f=__disk__c9%2F40%2Ff2%2Fc940f2f914e04b26bf0e81d81c9f273113f7a5c8"
 
   if (!dir.exists("Dataset3_DamRemovals")){
     dam_data <- use_zip(
       dam_url,
-      destdir = getwd()#,
-      #cleanup = if (rlang::is_interactive()) NA else FALSE
+      destdir = getwd()
     )
     dams <- read.table("Dataset3_DamRemovals/DamRemovals.txt",sep=",",header=T) %>%
       mutate(STAID = as.character(paste0("0", STAID))) %>%
@@ -82,37 +66,13 @@ dam_trends <- function(site_num, startDate, endDate){
 #' @return returns a data frame with 10 variables which includes the drainage area and site classification
 #' @export
 #'
-#' @example
-#' returns the identification of the basin for specified sites
+#' @examples
+#' # returns the identification of the basin for specified sites
 #' library(easyrbi)
 #' basin_id(c("01567000", "01490000", "01492500"))
 #'
 
-#basin_id <- function(site_num){
-  #if (nchar(site_num) < 8){
-    #stop("Invalid site number")
-  #} else{
-
-
-  #basin_url <- "https://www.sciencebase.gov/catalog/file/get/59692a64e4b0d1f9f05fbd39?f=__disk__3b%2F5c%2F06%2F3b5c0605384344f93b61c00fccf1a304b96019e3"
-  #basin_data <- use_zip(
-    #basin_url,
-    #destdir = getwd()
-
-  #)
-
-  #basins <- read.table("Dataset1_BasinID/BasinID.txt",sep=",",header=TRUE) %>%
-    #filter(STAID %in% c(site_num)) %>%
-    #as_tibble()
-  #return(basins)
-  #}
-#}
-
 basin_id <- function(site_num){
-  #if (nchar(site_num) < 8){
-    #stop("Invalid site number")
-  #} else{
-
     basin_url <- "https://www.sciencebase.gov/catalog/file/get/59692a64e4b0d1f9f05fbd39?f=__disk__3b%2F5c%2F06%2F3b5c0605384344f93b61c00fccf1a304b96019e3"
     if (!dir.exists("Dataset1_BasinID")){
       basin_data <- use_zip(
@@ -129,6 +89,5 @@ basin_id <- function(site_num){
         filter(STAID %in% c(site_num)) %>%
         as_tibble()
       return(basins2)
-    #}
   }
 }
