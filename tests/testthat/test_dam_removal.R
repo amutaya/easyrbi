b6a3c2e0a9f0dcca63be72705d985536993ae75c
wndlovu <wndlovu@smith.edu>
2022-05-04 10:23:07 -0400

    added a new test for basin_id

--- tests/testthat/test_dam_removal.R
+++ tests/testthat/test_dam_removal.R
@@ -16,3 +16,4 @@ expect_equal(ncol(dam_trends("01567000", "1970-10-01", "1990-09-30")), 21)
 # basinID
 #expect_error(nchar(basin_id("0120850")$STAID), "Invalid site number")
 expect_type((basin_id("01567000")$STATE), "character")
+expect_length(nchar(basin_id("0120850")$STAID), 0)
